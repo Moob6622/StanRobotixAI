9057ff50625f7ef6c74052285b25b75550af3a6e
Master <moob6622@gmail.com>
2018-01-27 11:19:07 -0500

    implemented bbox that are compatible with chainer

--- ImageLabelization.py
+++ ImageLabelization.py
@@ -16,23 +16,32 @@ class ImageLabelisation (object):
         self.base = []
         self.solution = []
         self.rectCoords  = [[[0,0,0,0]]]
-        self.index = int(0)
-        self.cubeIndex = int(0)
-        self.bbox = np.empty((self.cubeIndex, 4))
+        self.index = int(0)                #numero de click
+        self.cubeIndex = int(0)            #nombre de boite 
+        self.bboxDict = {}                 #boites formatees pour chainer
+        
     def Coord (self,event,x,y,param,i):
 
         if event == cv2.EVENT_LBUTTONDOWN:
             ##stockage des coordonnees
             self.rectCoords[i][self.cubeIndex][self.index] = x
             self.rectCoords[i][self.cubeIndex][self.index+1] = y
+            
             self.index = self.index +2
             
         if(self.index == 4) :
             ##affichage du rectangle
             cv2.rectangle(self.solution[i],(self.rectCoords[i][self.cubeIndex][0],self.rectCoords[i][self.cubeIndex][1]),(self.rectCoords[i][self.cubeIndex][2] ,self.rectCoords[i][self.cubeIndex][3]),(0,0,255),1,8,0)
+            
             ##update de limage
             cv2.imshow('Window'+str(i),self.solution[i])
-            ##update des arrays 
+            
+            ##update des arrays et du dico
+            
+            self.bboxDict['cube'+str(i+1)+'.jpg'] = np.append (self.bboxDict['cube'+str(i+1)+'.jpg'], [self.rectCoords[i][self.cubeIndex]], axis = 0)
+            #print(str(self.bboxDict['cube'+str(i+1)+'.jpg']) + '\n')
+            
+            
             self.index = 0
             self.rectCoords[i].append([0,0,0,0])
             self.cubeIndex  = self.cubeIndex + 1
@@ -43,19 +52,29 @@ class ImageLabelisation (object):
             self.base.append(cv2.imread('cube'+str(i+1)+'.jpg',1))
             self.base[i] = cv2.resize(self.base[i],(320,240), interpolation = cv2.INTER_CUBIC)
             self.solution.append(self.base[i])
-            ##update de larray cd
+            
+            ##update de l'array coord
             self.rectCoords.append([[0,0,0,0]])
+            
+            self.bboxDict['cube'+str(i+1)+'.jpg'] = np.zeros((0, 4))
+            
             ##affichage de limage
             cv2.imshow('Window'+str(i),self.base[i])
+            
             ##appel fonction dessin de carres + stockage de coords 
             cv2.setMouseCallback('Window'+str(i),self.Coord,i)
+            
             while(True) :
                 if cv2.waitKey(20) & 0xFF == 27:
                     break
             cv2.destroyAllWindows()
             self.cubeIndex = 0
+        return self.bboxDict
+
+        
             
 
 import ImageLabelization    
 x = ImageLabelization.ImageLabelisation(3)
-x.DrawRect()
+y = x.DrawRect()
+print(y)
