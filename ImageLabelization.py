tyring to fix the 'could not broadcast array'error - still unsuccesfull

ImageLabelization.py
@@ -35,31 +35,7 @@ class ImageLabelisation (object):
             cv2.imshow('Window'+str(i),self.solution[i])
             
             ##update des arrays et du dico
-##            x =[]
-##            y =[]
-##
-##            xp = []
-##            yp = []
-##
-##            for j in range(0,4):
-##                if i%2 == 0:
-##                    x.append(self.rectCoords[i][self.cubeIndex][j])
-##                    xp.append(self.rectCoords[i][self.cubeIndex][j])
-##                else :
-##                    y.append(self.rectCoords[i][self.cubeIndex][j])
-##                    yp.append(self.rectCoords[i][self.cubeIndex][j])
-##                    
-##            x.sort()
-##            y.sort()
-##
-##            if y!=yp :
-##                save =  self.rectCoords[i][self.cubeIndex][0]
-##                self.rectCoords[i][self.cubeIndex][0] = self.rectCoords[i][self.cubeIndex][2]
-##                self.rectCoords[i][self.cubeIndex][2] = save
-##            if x!=xp:
-##                save  = self.rectCoords[i][self.cubeIndex][1]
-##                self.rectCoords[i][self.cubeIndex][1] = self.rectCoords[i][self.cubeIndex][3]
-##                self.rectCoords[i][self.cubeIndex][3] = save
+
                      
             self.bboxDict['cube'+str(i+1)] = np.append (self.bboxDict['cube'+str(i+1)], [self.rectCoords[i][self.cubeIndex]], axis = 0)
             #print(str(self.bboxDict['cube'+str(i+1)+'.jpg']) + '\n')
@@ -74,7 +50,7 @@ class ImageLabelisation (object):
 ##                self.lbl = lblIndx
 
             self.lblDict['cube'+str(i+1)] = np.append (self.lblDict['cube'+str(i+1)], [[self.lbl]], axis = 0)
-            
+            #self.lblDict['cube'+str(i+1)].append(self.lbl)
                 
 
             
@@ -95,6 +71,7 @@ class ImageLabelisation (object):
             ##update de l'array lbl
 
             self.lblDict['cube'+str(i+1)] = np.zeros((0, 1))
+            #self.lblDict['cube'+str(i+1)] = []
             
             ##affichage de limage
             cv2.imshow('Window'+str(i),self.base[i])
@@ -114,7 +91,7 @@ class ImageLabelisation (object):
 if __name__ == '__main__' :
     import ImageLabelization
 
-    x = ImageLabelization.ImageLabelisation(4)
+    x = ImageLabelization.ImageLabelisation(3)
     bbox, lbl = x.DrawRect()
     np.savez('./data/bboxs' , *list(bbox.values()), **bbox)
     np.savez('./data/lbls' , *list(lbl.values()), **lbl)

Train.py
@@ -70,13 +70,11 @@ class DataFeeder (object) :
         bbox     = bboxDat[imgPath].astype(np.float32)
         lbl      = lblDat[imgPath]
 
-        isImg = utils.assert_is_image(img)
-        print(isImg)
-        isBbox = utils.assert_is_bbox(bbox)
-        print(isBbox)
-
+        
+        print(utils.assert_is_image(img))
         # 1. Augmentation de couleur
         img = random_distort(img)
+        print(utils.assert_is_image(img))
 
         # 2. Expansion aléatoire
 
@@ -86,7 +84,7 @@ class DataFeeder (object) :
             
             bbox = transforms.translate_bbox(
                 bbox, y_offset=param['y_offset'], x_offset=param['x_offset'])
-
+        print(utils.assert_is_image(img))
         # 3. Coupage aléatoir
 
         img, param = random_crop_with_bbox_constraints(
@@ -97,26 +95,27 @@ class DataFeeder (object) :
             allow_outside_center=False, return_param=True)
          
         lbl = lbl[param['index']]
-
+        print(utils.assert_is_image(img))
         # 4. Redimensionner avec interpolation aléatoire
 
-        _, H, W = img.shape
+##        _, H, W = img.shape
+##        
+##        img = resize_with_random_interpolation(img, (self.size, self.size))
+##        
+##        bbox = transforms.resize_bbox(bbox, (H, W), (self.size, self.size))
+##        print(utils.assert_is_image(img))
         
-        img = resize_with_random_interpolation(img, (self.size, self.size))
-        
-        bbox = transforms.resize_bbox(bbox, (H, W), (self.size, self.size))
-
         # 5. Random horizontal flipping
         img, params = transforms.random_flip(
             img, x_random=True, return_param=True)
         
         bbox = transforms.flip_bbox(
             bbox, (self.size, self.size), x_flip=params['x_flip'])
-
+        print(utils.assert_is_image(img))
         # Formatation pour le réseaux SSD
-        img -= self.mean
+##        img -= self.mean
         mb_loc, mb_label = self.coder.encode(bbox, lbl)
-
+        print(utils.assert_is_image(img))
         return img, mb_loc, mb_label
         
             
@@ -124,7 +123,7 @@ class DataFeeder (object) :
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', default = './data')
-    parser.add_argument('--basesize', type = int, default = 4)
+    parser.add_argument('--basesize', type = int, default = 3)
     parser.add_argument('--size', type = int, default = 20)
     parser.add_argument(
         '--model', choices=('ssd300', 'ssd512'), default='ssd300')
@@ -158,7 +157,14 @@ def main():
     train =[]
     for i in range (0, args.size) :
         print(i)
-        train.append(feeder(args.path, i%args.basesize))
+        datum = feeder(args.path, i%args.basesize)
+        x,_,_ =datum
+        utils.assert_is_image(x,color=True)
+        train.append(datum)
+        
+    print (train[0])
+    utils.assert_is_bbox_dataset(train, 1)
+    utils.assert_is_label_dataset(train,1)
         
     trainIter = chainer.iterators.MultiprocessIterator (train, args.batchsize)
 